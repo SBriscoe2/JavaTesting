296ccabd6b8a1411739830c7bb5cc53065edce91
SBriscoe2 <shabris@tiscali.co.uk>
2019-01-14 12:30:01 +0000

    add java practices

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static String msg (String string) {
+        return string;
+    }
+
+    public static void main(String[] args) {
+
+	System.out.println("Hello World");
+    //Main main = new Main();
+    //System.out.println(main("Howdy World"));
+
+
+    }
+}
